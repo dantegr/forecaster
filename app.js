b92b20e9e3c23a5b1c4d2bc5c72d92ce5f769545
Pantelis <tampantelis@gmail.com>
2019-05-08 20:42:41 +0300

    add error handling to api

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const request = require('request');
 
-let lat;
-let long;
-
 const locationUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Thessaloniki.json?access_token=pk.eyJ1IjoiZGFudGVnciIsImEiOiJjanZhd2F1ZWowb3lkM3lxbWNwbXBmdW9lIn0.3gVF-ekrnv4XrVRJ4YBUmQ&limit=1';
 
 request({ url: locationUrl , json: true },(error, response) => {
+  if (error){
+    console.log('Unable to connect to geocoding service');
+  }else if (response.body.features.length === 0){
+    console.log('Unable to find loacation')
+  }else {
   const latitude = response.body.features[0].center[1];
-  lat = latitude;
+ 
   const longtitude = response.body.features[0].center[0];
-  long = longtitude;
+ console.log(latitude, longtitude);
+  }
 });
 
 
 
-const url = 'https://api.darksky.net/forecast/131c59f4dd9c0c2c3f1bca28f69c250d/'+ lat +','+ long +'?units=si';
+const url = 'https://api.darksky.net/forecast/131c59f4dd9c0c2c3f1bca28f69c250d/40.736851,22.920227?units=si';
 
 
 request({ url: url , json: true },(error, response) => {
-  console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is ' + response.body.currently.precipProbability +'% chance of rain' );
+  if (error){
+    console.log('unable to connect to weather service');
+  }else if (response.body.error) {
+    console.log('unable to find location');
+  }else {
+  console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + 'C degrees out. There is ' + response.body.currently.precipProbability +'% chance of rain' );
+  }
 });
