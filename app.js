59b5a0845f9b6e6036282afd7fe2646ae29bda6b
Pantelis <tampantelis@gmail.com>
2019-05-09 21:18:38 +0300

    setup darksky api callback function

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const request = require('request');
 
 const geocode = require('./utils/geocode');
-
-
-
-
-// const url = 'https://api.darksky.net/forecast/131c59f4dd9c0c2c3f1bca28f69c250d/'+ geocode +'?units=si';
-
-
-// request({ url: url , json: true },(error, response) => {
-//   if (error){
-//     console.log('unable to connect to weather service');
-//   }else if (response.body.error) {
-//     console.log('unable to find location');
-//   }else {
-//   console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + 'C degrees out. There is ' + response.body.currently.precipProbability +'% chance of rain' );
-//   }
-// });
-
-
+const darksky = require('./utils/darksky');
 
 
 geocode( 'Stockholm', (error,data) => {
   console.log('Error',error);
   console.log('Data',data);
+});
+
+darksky(40.63333, 22.95, (error,data) => {
+  console.log('Error:', error);
+  console.log('Data:',data);
 });
