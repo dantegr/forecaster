6f241624ea708b93fa61ba78617a26c4c72d942b
Pantelis <tampantelis@gmail.com>
2019-05-11 19:43:53 +0300

    set up dynamic templates with handlebars module

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
+
 
 console.log(__dirname);
 console.log(path.join(__dirname,'../public'));
@@ -7,7 +8,29 @@ console.log(path.join(__dirname,'../public'));
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname,'../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('',(req, res) => {
+  res.render('index',{
+    title: 'Weather App',
+    name: 'Pantelis Tamtakos'
+  });
+});
+
+app.get('/about',(req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name:'Pantelis Tamtakos'
+  });
+});
+
+app.get('/help',(req, res) => {
+  res.render('help', {
+    message: 'You need some help'
+  });
+});
 
 app.get('/weather',(req, res) => {
   res.send({forecast:'this the weather forecast', location:'Thessaloniki'});
