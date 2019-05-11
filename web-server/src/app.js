910e9acefd52aaa00f13c1b685317c5030eb2913
Pantelis <tampantelis@gmail.com>
2019-05-11 18:42:32 +0300

    set up links to ther site

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('',(req, res) => {
-  res.send('Hello world');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help',(req, res) => {
-  res.send('this is the help page');
+  res.send({name: 'Pantelis', age: 32});
 });
 
 app.get('/about',(req, res) => {
-  res.send('this is the about page');
+  res.send('<h1>This the abou page</h1>');
 });
 
 app.get('/weather',(req, res) => {
-  res.send('this is the weather');
+  res.send({forecast:'this the weather forecast', location:'Thessaloniki'});
 });
 
 app.listen(3000, () => {
