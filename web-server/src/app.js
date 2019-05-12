5db27eadd7e5a0e7420d7e222b47fc9925bf1f14
Pantelis <tampantelis@gmail.com>
2019-05-12 20:09:08 +0300

    created a 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -43,6 +43,20 @@ app.get('/weather',(req, res) => {
   res.send({forecast:'this the weather forecast', location:'Thessaloniki'});
 });
 
+app.get('/help/*',(req,res) => {
+  res.render('404', {
+    title: '404 Page',
+    error:'Help article not found'
+  });
+});
+
+app.get('*',(req,res) => {
+  res.render('404', {
+    title: '404 Page',
+    error:'Page not found'
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
