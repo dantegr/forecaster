73de92af76b3003cb4093d13d8c7ef80dece7bb8
Pantelis <tampantelis@gmail.com>
2019-05-14 19:40:16 +0300

    set up error for weather query

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,7 +40,29 @@ app.get('/help',(req, res) => {
 });
 
 app.get('/weather',(req, res) => {
-  res.send({forecast:'this the weather forecast', location:'Thessaloniki'});
+  if (!req.query.address) {
+    return res.send({
+      error:'Address must be provided'
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    forecast:'this the weather forecast',
+    location:'Thessaloniki',
+    address: req.query.address
+    });
+});
+
+app.get('/products',(req,res) => {
+  if (!req.query.search) {
+    return res.send({
+      error:'You must provide a search term'
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products:[]
+  })
 });
 
 app.get('/help/*',(req,res) => {
