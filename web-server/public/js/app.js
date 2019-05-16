27a4efa3a5252adcc7d512156189834a5818b039
Pantelis <tampantelis@gmail.com>
2019-05-16 21:01:50 +0300

    wired up user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,6 +4,10 @@ console.log('Clinet side js is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -15,10 +19,11 @@ weatherForm.addEventListener('submit', (e) => {
   fetch('http://localhost:3000/weather?address='+ location).then((response) => {
   response.json().then((data) => {
     if (data.error){
-      console.log(data.error);
+      messageOne.textContent = data.error;
+      messageTwo.textContent = '';
     }else{
-    console.log(data.location);
-    console.log(data.forecast);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     }
   });
 });
